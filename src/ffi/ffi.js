76a9d300109234d1259dcfe66a3f796c4fdd096d
xinghunm <xinghunm@curacloudcorp.com>
2023-08-15 14:23:52 +0800

    add ffi demo

---
+++ src/ffi/ffi.js
@@ -0,0 +1,10 @@
+const ffi = require("ffi-napi");
+const path = require("path");
+
+const libPath = path.join(__dirname, "math.dylib");
+
+const mathLib = ffi.Library(libPath, {
+  Add: ["double", ["double", "double"]],
+});
+
+console.log("add: 333 + 333 =", mathLib.Add(333, 333));

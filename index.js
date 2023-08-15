ebaa3faa173da69f21597bd3e728fba07239fb05
xinghunm <xinghunm@curacloudcorp.com>
2023-08-15 10:32:51 +0800

    add c++ file

---
+++ index.js
@@ -0,0 +1,3 @@
+const addon = require("./build/Release/math.node");
+
+console.log("addNumber: 1 + 2 =", addon.addNumber(1, 2));
---
+++ node-addon-api/math.cc
@@ -0,0 +1,30 @@
+#include <napi.h>
+#include<iostream>
+#include<string>
+using namespace Napi;
+//直接返回c++的数值类型数据到js
+Value getNumber(const CallbackInfo& info) {
+  Env env = info.Env();
+  int num = 2;
+  std::cout << "c++-log=>" << num << std::endl;
+  Number result = Number::New(env, num);
+
+  return result;
+}
+// 接收js传来的两个数值，转化为C++可用的数值后，相加并将结果返回。没有做参数的类型检测，传入字符串或不传，会报错
+Value addNumber(const CallbackInfo& info){
+  Env env = info.Env();
+  double arg0 = info[0].As<Number>().DoubleValue();
+  double arg1 = info[1].As<Number>().DoubleValue();
+  double num = arg0+arg1;
+  Number result = Number::New(env, num);
+  return result;
+}
+
+Object Init(Env env, Object exports) {
+  exports.Set(String::New(env, "getNumber"), Function::New(env, getNumber));
+  exports.Set(String::New(env, "addNumber"), Function::New(env, addNumber));
+  return exports;
+}
+
+NODE_API_MODULE(addon, Init)
